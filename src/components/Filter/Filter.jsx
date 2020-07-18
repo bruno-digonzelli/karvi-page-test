825d7e312b3a3f9492a92bc168f220a0454d32c0
Bruno <bruno@wcanvas.com>
2020-07-17 21:07:14 -0300

    #2 - Add filter module styles desktop

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -4,9 +4,11 @@ import LocationIcon from '../../../public/images/icon-location.svg';
 const Filter = () => {
   return (
     <section className="filter">
-      <div className="filter__results-total">
-        <h5>4.324 resultados</h5>
+
+      <div className="filter__results-total-container">
+        <h6 className="filter__results-total">4.324 resultados</h6>
       </div>
+
       <ul className="filter__results-container">
 
         <li className="filter__results-item d-flex align-items-center justify-content-between">
@@ -14,17 +16,22 @@ const Filter = () => {
           <h5 className="filter__results-title">Localização</h5>
           <div className="filter__results-geo-container">
 
-            <span className="link">
-              Sao Paulo
+            <button className="link filter__results-geo d-flex align-items-end">
+
+              <h6 className="filter__results-geo-text">São Paulo</h6>
               <img src={LocationIcon} alt="Location"/>
-            </span>
+
+            </button>
+
 
           </div>
 
         </li>
 
       </ul>
-    </section> );
+
+    </section>
+  );
 };
 
 export default Filter;
