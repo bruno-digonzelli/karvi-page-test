824526ea4855d4a43ae1f36005575a2a68ea1bbd
Bruno <bruno@wcanvas.com>
2020-07-18 21:23:51 -0300

    #3 - Add redux actions , reducer, types

---
+++ src/actions/products/getProductsActions.jsx
@@ -0,0 +1,35 @@
+import {
+  GET_PRODUCTS,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+} from '../../types';
+import axiosClient from '../../config/axios';
+
+export function getProductsActions() {
+  return async (dispatch) => {
+    dispatch( getProducts(true) );
+
+    try {
+      const response = await axiosClient.get('/products');
+      dispatch( getProductsSuccess( response.data ) );
+    } catch (error) {
+      console.log(error);
+      dispatch( getProductsError(true) );
+    }
+  };
+}
+
+const getProducts = (loading) => ({
+  type: GET_PRODUCTS,
+  payload: loading,
+});
+
+const getProductsSuccess = (products) => ({
+  type: GET_PRODUCTS_SUCCESS,
+  payload: products,
+});
+
+const getProductsError = (error) => ({
+  type: GET_PRODUCTS_ERROR,
+  payload: error,
+});
---
+++ src/reducers/productsReducer.jsx
@@ -0,0 +1,36 @@
+import {
+  GET_PRODUCTS,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+} from '../types';
+
+const initialState = {
+  products: [],
+  filters: [],
+  loading: false,
+  error: null,
+};
+
+export default function( state = initialState, action) {
+  switch (action.type) {
+  case GET_PRODUCTS:
+    return {
+      ...state,
+      loading: action.payload,
+    };
+  case GET_PRODUCTS_SUCCESS:
+    return {
+      ...state,
+      products: action.payload.items,
+      filters: action.payload.availableFilters,
+    };
+  case GET_PRODUCTS_ERROR:
+    return {
+      ...state,
+      loading: initialState.loading,
+      error: action.payload,
+    };
+  default:
+    return state;
+  }
+}
